4627bb0dacbca543666e8755cc74e445f2b6e718
unknown <diebraga0055@gmail.com>
2020-11-20 10:55:55 +0000

    experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -9,10 +9,58 @@ const Experience = () => {
         <div className='my-auto pt-5 p-5'>
           <h1 className='mt-5'>Work Experience</h1>
           <hr/>
-        <p className='mt-2'>
-        
-        </p>
-
+          <div className='p-2 ml-2'>
+          <h3>Teaching Experience</h3>
+          <p className='mt-2'>
+      Here are some of my most recent experiences, all within the <strong>University of Limerick (UL)</strong>, Ireland. To view the complete list of my previous roles and employers as well as the exact dates, please check my CV attached at the end of this portfolio. 
+UL's School of Education is the largest post-primary teacher education provider in the state and is ranked in the top 150 universities in the world for Education programmes. University of Limerick is also ranked within the top 75 universities in Europe for excellence in teaching and learning. 
+      </p>
+      <br/>
+      <h4>MODULE LEADER AND LECTURER </h4>
+      
+      <p>
+      EN4022: EDUCATIONAL TECHNOLOGY FOR TEACHING AND LEARNING <br/>
+      This undergraduate module introduces students to various forms of educational technology providing them with the practical and pedagogical skills to integrate these technologies into their teaching. A core focus of the module is to empower participants to capitalise on the personal, social and educational benefits of technologies whilst recognising the critical questions raised by an increasingly technological society. As a module leader, I taught two hours of lectures to a group of approximately 280 students who would gather in an auditorium twice a week. In addition, I managed a team of two lab instructors who were responsible for teaching two hours of EN4022 tutorials to smaller groups of students. All exams, assignments and how each of them should be graded were also decided by the module leader. I had to deal with any issues concerning students and/or their performance in the module.
+      </p>
+      EN6010: RESEARCH METHODS I
+      <br/>
+      
+      This post-graduate module introduces students to educational research and its contribution to policy and practice. It outlines the different paradigms of research and supports students in exploring examples of significant research studies that have emerged from different perspectives. The module also presents a range of different research methodologies with examples. During this module, students begin to plan their overall research design for their thesis with ethical and practical considerations. As a module leader, I taught two hours of lectures to a group of approximately 90 students who would gather in an auditorium twice a week. All exams, assignments and how each of them should be graded were also responsibilities of mine. I had to deal with any issues concerning students and/or their performance in the module.
+    <br/>
+    <br/><br/>
+    <h4>TUTOR</h4>
+    
+    <p>
+    EN4032: UNDERSTANDING YOUNG PEOPLE AND HOW THEY LEARN <br/>
+    This undergraduate module introduces students to key concepts in developmental and educational psychology. It provides students with a critical understanding of key learning theories, examining behavioural, cognitive and constructivist theories from both a historical and contemporary perspective. It also provides a critical understanding of relevant aspects of adolescent development and their applications to teaching, learning and assessment. As a tutor for this module, I offered two hours of teaching every week to several groups of approximately 30 students each. The tutorials were conducted in a way to promote active engagement of students through group work, reflection and discussion. I was also involved in the grading process.
+    <br/><br/>
+    EN6010: RESEARCH METHODS I
+    <br/>
+    This post-graduate module introduces students to educational research and its contribution to policy and practice. It outlines the different paradigms of research and supports students in exploring examples of significant research studies that have emerged from different perspectives. The module also presents a range of different research methodologies with examples. During this module, students begin to plan their overall research design with ethical and practical considerations. As a tutor for this module, I offered two hours of teaching every week to groups of approximately 30 students each. The tutorials were conducted in a way to promote active engagement of students through reflection and discussion. 
+    <br/><br/>
+    EN4043: UNDERSTANDING CLASSROOM PRACTICES
+    <br/>
+    This undergraduate module focuses on the development of knowledge, skills, and attitudes which will support student teachers in preparing for their School Placement (SP) by developing their capacity to engage in and reflect upon effective planning, preparation and management of learning environments. Contents discussed include Evidence Based Practice; Teacher as Researcher; Pedagogical Strategies; Classroom Management; Assessment for/of learning; benefits and limitations of using statistical analysis strategies to determine the effectiveness of pedagogical approaches. This module helps students to understand schools and the dominant teaching approaches used in Ireland. It also prepares students to plan and effectively facilitate student learning in ways to maximise pupil learning. As a tutor for this module, I offered two hours of teaching every week to several groups of approximately 30 students each. The tutorials were conducted in a way to promote active engagement of students through group work, reflection and discussion. I was also involved in the grading process.
+    <br/><br/>
+    EN4041: CONTEMPORARY UNDERSTANDING & THINKING ON EDUCATION
+    <br/>
+    During this undergraduate module students are exposed to some of the major contemporary thinkers in education. They are encouraged to critically analyse theory through the lens of deconstruction of their own very recent experiences of schooling. It discusses the interplay between educational theory and practice. This module aims to foster an understanding of teacher identity through critical engagement with the nature and purpose of education. As a tutor for this module, I offered two hours of teaching every week to several groups of approximately 30 students each. The tutorials were conducted in a way to promote active engagement of students through group work, reflection and discussion. I was also involved in the grading process.
+    <br/><br/>
+    EN6094: REPORTING EDUCATIONAL RESEARCH
+    <br/>
+    The purpose of this post-graduate module is to assist students in completing their research article associated with the research project they have engaged in during their teaching practice placements. It also aims to assist the students in analysing and interpreting the research data they have collected, and presenting the findings to their peers. As a tutor for this module, I offered two hours of teaching every week to two groups of approximately 25 students each. The tutorials were conducted in a way to promote active engagement of students through research, reflection and discussion. I was also involved in the grading of their research article at the end of the semester.
+    <br/><br/>
+    For more details on each module taught, please access: <br/><a className='' href='https://bookofmodules.ul.ie/' target='_blank' rel="noopener noreferrer">https://bookofmodules.ul.ie/</a>
+    <br/><br/>
+    <strong>INTERIM COURSE DIRECTOR – LM091: BACHELOR OF EDUCATION IN LANGUAGES</strong>
+    <br/>
+    The aim of this four-year programme is to educate teachers and equip them with the skills and aptitudes for a successful career as a languages teacher. Graduates from the programme are qualified to teach two chosen languages in Ireland at both Junior Certificate and Leaving Certificate levels. The languages students can choose from are French, German, Japanese, Spanish and Irish. I dealt with queries from future students as applications for the first year of this programme were about to start when I was the interim course director. My most important contribution while in this position was to lead the Teaching Council accreditation of the programme (the professional body for teaching in Ireland, responsible for regulating the teaching profession and promoting professional standards in teaching). Important and thorough documentary evidence about the programme was produced, reviewed and made available to the Teaching Council to demonstrate that the programme was appropriate for accreditation. It was an extremely detailed work that was successfully completed with the support of a team of experts managed by me. In relation to this team, it was formed by experienced professionals such as course and department leaders from three different faculties.
+    <br/><br/>
+    For more details on the ‘LM091: Bachelor of Education in Languages’, please access: 
+    <br />
+    <a className='' href='https://www.ul.ie/courses/bachelor-education-languages' rel="noopener noreferrer">https://www.ul.ie/courses/bachelor-education-languages</a>
+    </p>
+        </div>
         </div>
         </div>
         </Fade>
