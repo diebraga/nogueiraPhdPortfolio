48e3cc9a7657fb661da3f66921b7492fa4995b5d
unknown <diebraga0055@gmail.com>
2020-11-20 14:51:25 +0000

    professional

--- src/pages/Professional.js
+++ src/pages/Professional.js
@@ -9,10 +9,61 @@ const Professional = () => {
         <div className='my-auto pt-5 p-5'>
           <h1 className='mt-5'>Professional development</h1>
           <hr/>
-        <p className='mt-2'>
+          <div className='p-2 ml-2'>
+          <h3>PRESENTATIONS</h3>
+          <p className='mt-4'>
+          2019. <strong className='font-weight-bold'>Promoting 21st century learning: The use of technology to enhance problem-design, collaboration, feedback and assessment in Problem Based Learning (PBL).</strong> Limerick, Ireland.
+        <br/><br/>
+        2015. <strong className='font-weight-bold'>VIII Intelligent Technologies & Digital Design Research Meeting.</strong> São Paulo, Brazil.
+        <br/><br/>
+        2015. <strong className='font-weight-bold'>IV Web Curriculum Seminar & XII Curriculum Researchers Meeting.</strong> São Paulo, Brazil.
+        <br/><br/>
+        2014. <strong className='font-weight-bold'>English Language Teaching Congress.</strong> Campinas, Brazil.
+        <br/><br/>
+        2014. <strong className='font-weight-bold'>XVII Symposium of Education of Rede Azul.</strong> São Paulo, Brazil. 
+        <br/><br/>
+        2014. <strong className='font-weight-bold'>IV Distance Education Journey: the future of art.</strong> São Paulo, Brazil. 
+        <br/><br/>
+        2013. <strong className='font-weight-bold'>International ABED Congress of Distance Education.</strong> Salvador, Brazil. 
+        <br/><br/>
+        2013. <strong>IV International Congress of ABT Educational Technology.</strong> São Paulo, Brazil.
+        <br/><br/><br/>
+        <h4>ONLINE COURSES</h4>
+        <br/>
+        2020. <strong className='font-weight-bold'>Teaching English Online</strong><br/>
+        Cambridge Assessment English. Credit hours: 20.
+        <br/><br/>
+        2020. <strong className='font-weight-bold'>Inside IELTS: Preparing for the Test With the Experts</strong><br/>
+        IELTS, British Council, Cambridge Assessment English, idp. Credit hours: 15.
+                <br/><br/>
+        2014. <strong className='font-weight-bold'>Digital Skills for Teachers</strong><br/>
+        ECO imooc (UAB). Credit hours: 25. 
+        <br/><br/>
+        2013. <strong className='font-weight-bold'>E-learning and Digital Cultures</strong><br/>
+        University of Edinburgh. Credit hours: 35.
+                <br/><br/>
+        2012. <strong className='font-weight-bold'>Fantasy & Science-Fiction</strong><br/>
+        University of Michigan. Credit hours: 168.
+                <br/><br/><br/>
+        <h4 className='font-weight-bold'>FACE-TO-FACE COURSES</h4>
+        <br/>
+        2019. <strong className='font-weight-bold'>Educational Technology Bootcamp: Turnitin, BigBlueButton, PowerPoint, Office 365 Collaboration Tools, Adobe Spark.</strong>
+        University of Limerick. Ireland. Credit hours: 24.
+        <br/><br/>
+        2013. <strong className='font-weight-bold'>Tutor’s Role in an Online Course</strong><br/>
+        ABED – Associação Brasileira de Educação a Distância. Salvador, Brazil. Credit hours: 6
+                        <br/><br/>
+        2010. <strong className='font-weight-bold'>Supplementary/Alternative Communication</strong><br/>
+        PUCCAMP. Campinas, Brazil. Credit hours: 34. 
+                        <br/>
+                        <br/>
+        2009. <strong className='font-weight-bold'>New Paradigms for Social Inclusion</strong><br/>
+        PUCCAMP. Campinas, Brazil. Credit hours: 17.
+
+
         
         </p>
-
+        </div>
         </div>
         </div>
         </Fade>
