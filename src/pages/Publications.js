798772724dfd4c533bc5413d0d752de7ab824f85
unknown <diebraga0055@gmail.com>
2020-11-20 12:45:45 +0000

    publications page

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -10,7 +10,44 @@ const Publication = () => {
           <h1 className='mt-5'>Publications</h1>
           <hr/>
         <p className='mt-2'>
-        
+        <h6 className='font-weight-bold'>Nogueira, B. M.</h6>
+        (2019) ‘Teaching English as a Second Language to High School students with the support of Facebook and other digital resources’. VI Seminário Web Currículo e XII Encontro de Pesquisadores em Currículo, São Paulo, Brazil, 4-6 Nov. Available: 
+        <br/>
+        <a style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} href="https://www.academia.edu/42155271/Anais_do_VI_Semin%C3%A1rio_Web_Curr%C3%ADculo_educa%C3%A7%C3%A3o_e_humanismo" target='_blank' rel="noopener noreferrer">https://www.academia.edu/42155271/Anais_do_VI_Semin%C3%A1rio_Web_Curr%C3%ADculo_educa%C3%A7%C3%A3o_e_humanismo</a>
+        <br/><br/><br/>
+        <h6 className='font-weight-bold'>Nogueira, B. M.</h6>
+        (2016) ‘Education in the contemporary context and the complex thinking’, unpublished dissertation (MSc). Programme of Post-Graduate Studies of Intelligent Technology and Digital Design, Pontifícia Universidade Católica de São Paulo. Available:         <br/>
+        <a style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} href="https://tede2.pucsp.br/handle/handle/18962" target='_blank' rel="noopener noreferrer">https://tede2.pucsp.br/handle/handle/18962</a>
+        <br/><br/><br/>
+        <h6 className='font-weight-bold'>Nogueira, B. M.</h6>
+        (2015) ‘Challenges of the connectivism as a democratic learning theory’. IV Seminário Web Currículo e XII Encontro de Pesquisadores em Currículo, São Paulo, Brazil, 21-23 Sep. Available: 
+        <br/>
+        <a style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} href="http://www.pucsp.br/webcurriculo/downloads/anais/anais_iv-webcurriculo_2015.pdf" target='_blank' rel="noopener noreferrer">http://www.pucsp.br/webcurriculo/downloads/anais/anais_iv-webcurriculo_2015.pdf</a>
+        <br/><br/><br/>
+        <h6 className='font-weight-bold'>Nogueira, B. M., Leite, S. A. S.</h6>
+        (2015) ‘Study explores the importance of affectivity in the practices of research supervision’ [press release].  Revista de Educação da PUC Campinas. Available: 
+        <br/>
+        <a style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} href="http://periodicos.puc-campinas.edu.br/seer/index.php/reveducacao/article/view/2867/1959" target='_blank' rel="noopener noreferrer">http://periodicos.puc-campinas.edu.br/seer/index.php/reveducacao/article/view/2867/1959</a>
+        <br/><br/><br/>
+        <h6 className='font-weight-bold'>Nogueira, B. M., Leite, S. A. S.</h6>
+        (2014) ‘Affectivity in the process of research supervision’. Revista de Educação da PUC Campinas, v. 19, p. 249-259. Available:        <br/>
+        <a style={{overflowWrap: 'break-word', wordWrap: 'break-word'}} href="http://periodicos.puc-campinas.edu.br/seer/index.php/reveducacao/article/view/2852" target='_blank' rel="noopener noreferrer">http://periodicos.puc-campinas.edu.br/seer/index.php/reveducacao/article/view/2852</a>
+        <br/><br/><br/>
+        <h4>COURSE MATERIAL</h4>
+        <h6 className='font-weight-bold'>Nogueira, B. M.</h6>
+        (2019) ‘Understanding connectivism’, EN4022: Educational Technology for Teaching and Learning, University of Limerick, unpublished. Available:
+         &nbsp;<a href=""> Here .</a>
+
+        </p>
+        <h3 className='pt-5'>Blog Articles</h3>
+        <p className='mt-2'>
+        As a master’s degree student, I used to keep a personal blog about education in the years of 2014 and 2015 (http://tecsedu.blogspot.com/). There, I wrote comments and summaries of my readings, shared useful teaching tools and various other pieces of information that I found useful or interesting within  my area of study. That experience was important to help me stay informed and develop my writing and critical thinking skills. I wrote texts both in English and Portuguese.
+        <br/><br/>One of the learning theories that I was particularly interested in at that time was connectivism, by George Siemens and Stephen Downes. After publishing online one of my texts on the topic, I noticed a great number of accesses to my page and found out that Downes himself had not only read my article, but also shared it on his own webpage – the reason why so many people were attracted to my blog. This was a very exciting day and it encouraged me to continue my studies and reflections in the area of education. 
+        <br/>
+        <a href="">Here </a>is the link to the mentioned article that was accessed by the Downes:
+        <br/><br/>
+        <strong className='font-weight-bold'>Is Connectivism A New Learning Theory Based on Old Ideas?</strong><br/>
+        <a href="http://tecsedu.blogspot.com/2015/02/is-connectivism-new-learning-theory.html" target='_blank' rel="noopener noreferrer">http://tecsedu.blogspot.com/2015/02/is-connectivism-new-learning-theory.html</a>
         </p>
 
         </div>
