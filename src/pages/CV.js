819098073be1d64951e6569e2da01485fbb86aeb
unknown <diebraga0055@gmail.com>
2020-11-20 15:06:17 +0000

    cv

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Fade } from "react-awesome-reveal";
-
+import {AiOutlineLink} from 'react-icons/ai'
 const CV = () => {
   return (
     <>
@@ -10,7 +10,18 @@ const CV = () => {
           <h1 className='mt-5'>CV & IELTS</h1>
           <hr/>
         <p className='mt-2'>
-        
+        <h4>CV</h4>
+
+        <a href='https://drive.google.com/file/d/1jY6Kr2S92dpzN7ZcDwy1Od_mEN_E4BgW/view' target='_blank' rel="noreferrer" download>(Attachment) <AiOutlineLink size={20}/></a>
+        <br/>
+        <br/>
+        <h4>IELTS</h4>
+        <hr/>
+        <a href='https://drive.google.com/file/d/13d0FoJvZlvrxZkOSuSxRsDTohySQWqAu/view' target='_blank' rel="noreferrer">GENERAL TRAINING (8.5 overall score) <AiOutlineLink size={20}/></a>
+        <br/>
+        <br/>
+        <a href='https://drive.google.com/file/d/11sQULyIR1D55EjzeuDoQ8hKcBZoEldbr/view' target='_blank' rel="noreferrer">ACADEMIC TRAINING (7.0 overall score) <AiOutlineLink size={20} /></a>
+
         </p>
 
         </div>
