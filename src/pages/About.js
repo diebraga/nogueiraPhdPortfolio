81b9fd29ff13e0588807450c211a6f0c923b5c51
unknown <diebraga0055@gmail.com>
2020-11-20 10:43:13 +0000

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Fade } from "react-awesome-reveal";
-
+import {ImPlus} from 'react-icons/im'; 
 const About = () => {
   return (
     <>
@@ -9,9 +9,67 @@ const About = () => {
         <div className='my-auto pt-5 p-5'>
           <h1 className='mt-5'>About</h1>
           <hr/>
-        <p className='mt-2'>
-        
-        </p>
+          <div className='acordion mt-4' id="accordionExample">
+  <div className="p-3">
+    <div className="" id="headingTwo">
+      <h2 className="mb-0">
+        <button className="btn btn-outline-dark btn-block text-left collapsed p-4 mb-4" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        BRUNA NOGUEIRA AS AN EDUCATOR <ImPlus className='ml-5' size={30} />
+                </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div className="pt-3">
+      I started teaching English as a second language (ESL) in 2008 in Brazil, the same year that I started my Bachelor of Psychology at Pontifícia Universidade Católica de Campinas. From that moment on, I have not stopped teaching. I have taught English and Portuguese as second languages to students from many different countries and cultures, of all age groups and proficiency levels. I have also taught students with learning difficulties or disabilities, as well as students from risk groups, like those addicted to drugs or from economically disadvantaged families. I have worked as an ESL teacher both in Brazil and in Ireland, in schools, language centres, companies, universities and online with private students. 
+      <br/><br/>
+      In addition to teaching languages, I have had the opportunity to work with one of my professors while an undergraduate student and mentor first year students. As a mentor, I assisted the professor in tutorials and helped with the correction of exams and activities. All these experiences have brought me a deeper understanding of classroom management and effective practices, teaching strategies, learning processes and, most importantly, how the environment and teacher-student relationships affect students’ progress. In other words, the more I work within education, the more I am convinced that there is no coherent dissociation between cognition and emotions when we deal with human beings. We are complex creatures who deserve to be seen in all our profusion and wealth.
+      <br/><br/>
+      Since moving to Ireland in 2017, I had the amazing opportunity to teach different modules to undergraduate and graduate students in the School of Education at the University of Limerick, one of Ireland’s most prestigious universities. Here, I have been lucky to experience academia from a new perspective. The outcome has been highly positive. I have created great connections and felt respected and valued as a professional. Here in the University of Limerick, I had the confirmation within me that this is what I want to do and that academia is where I want to stay. Therefore, I feel ready and excited to challenge myself with a PhD in Learning Sciences at the Werklund School of Education, University of Calgary, as it will allow me to navigate through new ideas and towards new horizons.       <br/><br/>
+
+      <br/><br/>
+      My goal is not to reach a destination, but to keep on sailing further day by day, opening and expanding my mind as much as possible, inspiring and motivating others not to follow me, but to follow their own dreams with the same enthusiasm, courage and passion. 
+      <br/><br/>
+
+      </div>
+    </div>
+  </div>
+  <div className="p-3">
+    <div className="" id="headingThree">
+      <h2 className="mb-0">
+        <button className="btn btn-outline-dark btn-block text-left collapsed h1 p-4" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        BRUNA NOGUEIRA AS A RESEARCHER <ImPlus className='ml-5' size={30} />
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div className="pt-4">
+      My career in research started at the most prestigious Brazilian university, the Universidade Estadual de Campinas (UNICAMP). The year was 2010 and I was an undergraduate student of Psychology at the Pontifícia Universidade Católica de Campinas (PUCCAMP) when I started to discover my professional interests. With my engagement in the course, I developed particular interest in research that put together Education and Psychology, with an emphasis on constructivist theories. After extensive independent readings, I was led to Dr Sérgio Leite, who worked at UNICAMP and had many publications that were relevant and aligned with my ideas. We met that same year and, in 2011, we agreed to develop a research project together. We applied for a grant from the most important funding agency of the state known as FAPESP, and the request was approved in 2012. Following that, we began to develop the research that same year. 
+      <br/><br/>
+      It was in the production process of my first research project, together with my growing experience in educational settings working as an ESL teacher that more ideas for future research emerged. The year after I finished my undergraduate studies, I attended conferences and seminars on education, networked with experts, read as much as I could and decided on the topic of my Master’s research: digital technologies and education. 
+      <br/><br/>
+      With that in mind, I applied to the MSc programme of Intelligent Technologies & Digital Design at the Pontifícia Universidade Católica de São Paulo (PUC-SP). Upon acceptance to the programme, I moved to São Paulo, where I lived for two years.
+      <br/><br/>
+      In 2017, I finished my Master’s degree, and got a job in Ireland to work as a researcher in finance for a private company. I moved to the European island and had the opportunity of developing a different type of research.  This provided me with significant learning, and helped me improve as a professional. That experience brought me a deeper understanding of quantitative analysis and statistical analysis tools, for example. It also enhanced my abilities of working under the pressure of tight deadlines, as it was a very fast-paced environment. While still employed in this dynamic position, I applied to a teaching job at the University of Limerick (UL) and started working there in June 2017.
+      <br/><br/>
+      Once in UL, I started my networking and volunteered to be a research assistant on a project that was aligned with my area of interest (education and digital technology). My participation in that first research project was successfully completed in 2018, and I was hired by UL for various positions after that. I am still employed by them and currently work as research assistant on a project that is explained in more detail under ‘research experience’. 
+      <br/><br/>
+      I believe that my broad experience in different areas, teams, institutions and countries, together with my outstanding organization skills, my determination and my thirst for finding answers to questions that intrigue me contribute tremendously to my successful career as a researcher. Another advantage is that I can read fluently in Portuguese, English and Spanish. This is particularly useful when looking for resources and references for a study – especially if there is an intention of incorporating a multicultural approach and minimize bias.
+      <br/><br/>
+      Over the course of my career, I have worked many times as part of a close-knit team of researchers from different nationalities and areas of expertise. This has increased my confidence in communicating my research to those outside my field, and has improved my intercultural skills. It also made me prioritize a multidisciplinary approach whenever possible. As a research strand coordinator of the Professional Master of Education at the University of Limerick, I learnt how to problem solve, build positive relationships and to negotiate solutions. 
+      <br/><br/>
+      I have also excelled in my academic career within the role of a student. This is thanks to my passion for learning, combined with my tenacity, curiosity and outstanding organizational skills. After five intense years as an undergraduate student, I managed to finish the course with an overall score of approximately 9/10 (or 3.55 on a 4-point scale). A few years later, I finished my Masters with an even better performance and left the university with both a degree and an overall score of 9.5/10 (or 3.8 on a 4-point scale). Although these numbers seem important when building a career, I risk saying that the high scores are not there to prove that I have learned everything. Instead, the good use and understanding of what I had access to make me realize that there is so much more to dig. The deepest I go into my learnings the more I discover an infinity of possibilities, theories, contents, links, interpretations – and this is what fascinates me. This is also what attracts me to research. 
+      <br/><br/>
+      My intention is to continue developing research that focuses on the use of digital technologies for teaching and learning. Furthermore, I intend to focus upon research which adopts a realistic point of view, meaning the inclusion of both the positives and the negatives of the changes brought by the digital age to education. I am particularly interested in how to promote a better education of future teachers, allowing them to have a meaningful learning experience in the third level that leads to a more successful start and development of their careers. This does not mean abandoning traditional education in favour of newer theories, but knowing how to use old and new ideas effectively to enhance teaching practice and promote significant and long-lasting learning. I believe that this is achievable by promoting student teachers active engagement and by focusing on the development of their ability to reflect upon their own experiences. In a world that floods us with information all the time and does not offer us enough opportunities to analyse it carefully, being able to think critically has become essential.
+      <br/><br/>
+      I am attentive to detail, hard-worker, committed, critical and systematic. In addition, I have always enjoyed writing, an extremely important part of the life of a researcher. Apart from my skills and personality characteristics, the academia environment seduces me strongly. I look for a career where I have the opportunity of learning and improving myself constantly both by accepting challenges and by finding myself surrounded by intelligent, motivated and inspiring people that push me forward. 
+      <br/><br/>
+      I believe my unique experiences and skillset equip me well for a prosperous career in research. I have developed research that includes both quantitative and qualitative data analysis, but most of my experience adopted qualitative methodologies and analysis only. I believe that the combination of quantitative and qualitative data analysis can enrich discussions and I would be willing to and interested in adopting a mixed approach in future projects if that seemed to significantly strengthen the study. However, despite valuing the addition of quantitative data, I am a lot more comfortable with qualitative data analysis and would be more inclined to favour that on a project developed by me. Most research projects I was involved with in the area of education employed the use of semi-structured one-to-one interviews followed by transcriptions and thematic analysis - with the exception of my Master’s project, explained in more details under ‘research experience’.
+      <br/><br/>
+      In relation to educational psychology frameworks, I see important contributions that come from each of them and I avoid strongly positioning myself against any theory. Instead, I recognize their positives while critically analysing the aspects that could have been better developed in my opinion. I believe in the construction of knowledge through critical thinking, dialogue, creative production and active engagement of learners in their own learning processes. In addition, I am convinced that the environment/context is crucial for the learning to occur and it goes much beyond the school or the classroom. All experiences of a student can affect their learning and should be considered in that process. Previous learnings, diet, quality and amount of sleep, beliefs, culture, self-esteem, traumas, interests and life goals are just some of the many elements that play important roles in how much a student will engage and be able to acquire knowledge. Relationships with friends, teacher and family are equally relevant. That said, a social constructivist tendency is undeniable in my discourse and professional practice.
+      </div>
+    </div>
+  </div>
+</div>
 
         </div>
         </div>
